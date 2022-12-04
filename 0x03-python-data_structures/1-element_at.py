6592b9dd37571bb754776fc646e3b7a33fba358b
LovinaOsaigbovo <mails4vina@gmail.com>
2022-12-04 09:33:28 -0800

    Task 1 again

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     for idx in my_list:
         if idx < 0:
             return None
-        elif idx > len(my_list - 1):
+        elif idx > (len(my_list) - 1):
             return None
         else:
        return my_list[idx]
