c507a133d2e042c0e3df6b4a8205af4b741370ec
LovinaOsaigbovo <mails4vina@gmail.com>
2022-12-22 06:46:25 -0800

    task 1

---
+++ 0x06-python-classes/1-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+Square = __import__('1-square').Square
+
+
+
+my_square = Square(3)
+
+print(type(my_square))
+
+print(my_square.__dict__)
+
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+/** This file/directory was created to tackle problems on classes and objects
+ * All scripts are written in Python Programming Language
+ */
