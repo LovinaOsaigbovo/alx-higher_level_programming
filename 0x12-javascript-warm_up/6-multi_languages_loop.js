cb55c5fa31c93cd0d2aeee97634598e7c492b236
Lovina Osaigbovo <111295018+LovinaOsaigbovo@users.noreply.github.com>
2023-03-13 21:57:52 +0100

    Create 6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+ble File  9 lines (6 sloc)  198 Bytes
+
+#!/usr/bin/node
+
+// Prints 3 lines by using a loop
+
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
