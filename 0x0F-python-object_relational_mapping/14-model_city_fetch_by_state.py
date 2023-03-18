f99c088b0c81f910a3841b1cf35f52c833111396
Lovina Osaigbovo <111295018+LovinaOsaigbovo@users.noreply.github.com>
2023-03-18 17:49:24 +0100

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
-"""
-import sys
+"""This script will print the city names of those joined with state"""
+from sqlalchemy import create_engine
 from model_state import Base, State
-from model_city import City
-from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-
+import sys
+from model_city import City
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in (session.query(State.name, City.id, City.name)
-                     .filter(State.id == City.state_id)):
-        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2]) 
+    result = session.query(City, State).join(State).all()
+    for city, state in result:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.close()
