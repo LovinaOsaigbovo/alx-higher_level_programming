83d6dc716c41926fdf9c41787d714ba4ecfe8c74
LovinaOsaigbovo <mails4vina@gmail.com>
2023-03-18 08:53:35 -0700

    Hey

--- 0x0F-python-object_relational_mapping/README.md
+++
@@ -1,3 +0,0 @@
-/**
- * This directory contains python files with codes that contain solutions to problems on object relational mapping
- * /
---
+++ 0x13-javascript_objects_scopes_closures/test.js
@@ -0,0 +1,14 @@
+let numArgs = process.argv.length - 2;
+
+if (numArgs === 0) 
+	{
+	  console.log("No argument");
+	} 
+else if (numArgs === 1) 
+	{
+	  console.log("Argument found");
+	}
+else 
+	{
+	  console.log("Arguments found");
+	}	
