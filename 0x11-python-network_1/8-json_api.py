17bdeee5fde23d62cd8adef0150c8b562d9dce68
Lovina Osaigbovo <111295018+LovinaOsaigbovo@users.noreply.github.com>
2023-03-31 15:37:25 +0100

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
 """
-Python script that sends a POST request to the URL and
-to an URL with the letter as a parameter
+Module  sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
 """
 import requests
-import sys
+from sys import argv
 
 
-if __name__ == "__main__":
-    data = {'q': ""}
-
+def main(argv):
+    """
+    Script that takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter.
+    """
+    if len(argv) < 2:
+        q = ""
+    else:
+        q = argv[1]
+    payload = {'q': q}
+    url = "http://0.0.0.0:5000/search_user"
+    r = requests.post(url, data=payload)
     try:
-        data['q'] = sys.argv[1]
-    except:
-        pass
-
-    r = requests.post('http://0.0.0.0:5000/search_user', data)
-
-    try:
-        json_o = r.json()
-        if not json_o:
+        result = r.json()
+        if bool(result) is False:
             print("No result")
         else:
-            print("[{}] {}".format(json_o.get('id'), json_o.get('name')))
+            print("[{}] {}".format(result['id'], result['name']))
     except:
         print("Not a valid JSON")
+
+if __name__ == "__main__":
+    main(argv)
