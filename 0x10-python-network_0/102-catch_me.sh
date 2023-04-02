7d747ced059d48f3826efcf231197f2e947cb8c3
Lovina Osaigbovo <111295018+LovinaOsaigbovo@users.noreply.github.com>
2023-04-02 05:23:09 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message You got me!
-curl -sL -X PUT -d "user_id=98" -H "origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
